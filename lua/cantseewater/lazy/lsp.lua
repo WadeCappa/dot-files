9e13ae71b2b374ec366b614602d45dffd25a1dd0
Wade Cappa <wade.cappa@msn.com>
2025-09-02 09:05:10 -0700

    devle

--- lua/cantseewater/lazy/lsp.lua
+++ lua/cantseewater/lazy/lsp.lua
@@ -13,5 +13,9 @@ local plugins = {
 	{"neovim/nvim-lspconfig"},
 	{"hrsh7th/nvim-cmp"},
 	{"hrsh7th/cmp-nvim-lsp"},
+	{"mfussenegger/nvim-dap"},
+	{"leoluz/nvim-dap-go"},
+	{"nvim-neotest/nvim-nio"},
+	{"rcarriga/nvim-dap-ui"},
 }
 return plugins
