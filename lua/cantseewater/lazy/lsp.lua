69f6690aa51fd20949b6b74a56079a87f1bf6fa7
Wade Cappa <wade.cappa@msn.com>
2026-03-01 10:06:34 -0800

    refactor

--- lua/cantseewater/lazy/lsp.lua
+++ lua/cantseewater/lazy/lsp.lua
@@ -10,7 +10,68 @@ local plugins = {
 		},
 	},
 	{"mason-org/mason-lspconfig.nvim"},
-	{"neovim/nvim-lspconfig"},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			-- Add cmp_nvim_lsp capabilities settings to lspconfig
+			-- This should be executed before you configure any language server
+			local lspconfig_defaults = require('lspconfig').util.default_config
+			lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+				'force',
+				lspconfig_defaults.capabilities,
+				require('cmp_nvim_lsp').default_capabilities()
+			)
+
+			require("mason-lspconfig").setup()
+
+			-- golang
+			vim.lsp.config('gopls', {
+				settings = {
+					gopls = {
+						completeUnimported = true,
+					},
+				},
+			})
+			vim.lsp.enable('gopls')
+
+			-- python
+			vim.lsp.config('pyright', {
+				settings = {
+					pyright = {
+						python = {
+							analysis = {
+								autoImportCompletions = true,
+								typeCheckingMode = "strict",  -- "off", "basic", or "strict"
+								diagnosticMode = "workspace",  -- options: "openFilesOnly" or "workspace"
+							},
+						},
+					},
+				},
+			})
+			vim.lsp.enable('pyright')
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(event)
+					local opts = {buffer = event.buf}
+
+					vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+					vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+					vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+					vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+					vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+					vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+					vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+					vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+					vim.keymap.set({'n', 'x'}, '<leader>fm', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+					vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+				end,
+			})
+		end,
+	},
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = { "hrsh7th/cmp-nvim-lsp" },
